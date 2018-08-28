da8e98259e87889a826854d72da4252fb93c01bb
ed.lopez1905@gmail.com <ed.lopez1905@gmail.com>
2018-08-27 18:29:18 -0700

    upload

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Example 1 --- boolean comparison using a while statement.
+            //Console.WriteLine("Guess my age!");
+            //int ageNumber = Convert.ToInt32(Console.ReadLine());
+            //bool isAge = ageNumber == 29;
+
+
+            //while (!isAge)
+            //{
+
+            //    switch (ageNumber)
+            //    {
+
+            //        case 15:
+            //            Console.WriteLine("Dude, seriously? 15?");
+            //            Console.WriteLine("Guess my age!");
+            //            ageNumber = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 20:
+            //            Console.WriteLine("Nope. Try again!");
+            //            Console.WriteLine("Guess my age!");
+            //            ageNumber = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 25:
+            //            Console.WriteLine("Warmer. Try again!");
+            //            Console.WriteLine("Guess my age!");
+            //            ageNumber = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 28:
+            //            Console.WriteLine("Super close! Try again");
+            //            Console.WriteLine("Guess my age!");
+            //            ageNumber = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 29:
+            //            Console.WriteLine("Nice! Good job. You earned yourself a cookie!");
+            //            isAge = true;
+            //            break;
+            //        default:
+            //            Console.WriteLine("NOO!!!");
+            //            Console.WriteLine("Guess again!");
+            //            ageNumber = Convert.ToInt32(Console.ReadLine());
+            //            break;
+
+            //    }
+            //}
+            //Console.Read();                      
+
+
+            //Example 2-- - comparison using a do while statement.
+            //Console.WriteLine("Guess my age!");
+            //int ageNumber = Convert.ToInt32(Console.ReadLine());
+            //bool isAge = ageNumber == 29;
+
+            //do
+            //{
+            //    switch (ageNumber)
+            //    {
+
+            //        case 15:
+            //            Console.WriteLine("Dude, seriously? 15?");
+            //            Console.WriteLine("Guess it again!");
+            //            ageNumber = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 20:
+            //            Console.WriteLine("Nope.");
+            //            Console.WriteLine("TRY AGAIN!");
+            //            ageNumber = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 25:
+            //            Console.WriteLine("Warmer. Try again!");
+            //            Console.WriteLine("Guess!");
+            //            ageNumber = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 28:
+            //            Console.WriteLine("Super close! Try again");
+            //            Console.WriteLine("Guess my age!");
+            //            ageNumber = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 29:
+            //            Console.WriteLine("Nice! Good job. You earned yourself a cookie!");
+            //            isAge = true;
+            //            break;
+            //        default:
+            //            Console.WriteLine("NOO!!!");
+            //            Console.WriteLine("Guess again!");
+            //            ageNumber = Convert.ToInt32(Console.ReadLine());
+            //            break;
+
+            //    }
+            //}
+            //while (!isAge);
+            
+            //Console.Read();
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
